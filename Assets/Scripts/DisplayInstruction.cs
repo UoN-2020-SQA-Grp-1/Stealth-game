4bf34a2b6f069913d6ad37cb0ce0caaca3ae702a
Matthew Todd <matthewdb.todd@gmail.com>
2020-12-17 13:53:03 +0000

    Added text triggers for the tutorial level

---
+++ Assets/Scripts/DisplayInstruction.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayInstruction : MonoBehaviour
+{
+    public Text TextUI;
+    public string DisplayText;
+    public GameObject Trigger;
+    public enum OnTriggerEvent
+    {
+        SetActive,
+        SetInactive
+    };
+    public OnTriggerEvent EventType;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TextUI.gameObject.SetActive(true);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.gameObject.CompareTag("Player"))
+            return;
+        if (EventType == OnTriggerEvent.SetActive)
+        {
+            TextUI.text = DisplayText;
+            TextUI.gameObject.SetActive(true);
+        }
+        else
+            TextUI.gameObject.SetActive(false);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        TextUI.gameObject.SetActive(false);
+        Destroy(Trigger);
+    }
+}
